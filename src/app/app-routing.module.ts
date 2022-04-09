9a180633c67c66612ecd7d5d94d988073c1f421b
Npyndus <npundus@gmail.com>
2022-04-09 23:43:03 +0300

    - github pages added - routing change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,17 @@ import {RouterModule, Routes} from '@angular/router';
 const routes: Routes = [
     {
         path: '',
-        redirectTo: 'landing-module',
+        redirectTo: 'landing',
         pathMatch: 'full',
     },
     {
-        path: 'landing-module',
+        path: 'landing',
         loadChildren: () =>
             import('./modules/landing-module/landing.module').then((m) => m.LandingModule)
     },
     {
         path: '**',
-        redirectTo: 'landing-module'
+        redirectTo: 'landing'
     },
 ];
 
