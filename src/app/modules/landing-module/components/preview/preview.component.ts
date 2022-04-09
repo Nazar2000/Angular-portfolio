029bc65d760a32aee112e90b61b7772555a709d8
Npyndus <npundus@gmail.com>
2022-04-09 23:30:49 +0300

    - created fist page of landing - added preview section - created about me section

--- src/app/modules/landing-module/components/preview/preview.component.ts
+++ src/app/modules/landing-module/components/preview/preview.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-preview',
-  templateUrl: './preview.component.html',
-  styleUrls: ['./preview.component.scss']
+    selector: 'app-preview',
+    templateUrl: './preview.component.html',
+    styleUrls: ['./preview.component.scss']
 })
 export class PreviewComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+    }
 }
--- src/app/modules/landing-module/landing.module.ts
+++ src/app/modules/landing-module/landing.module.ts
@@ -4,11 +4,13 @@ import {CommonModule} from '@angular/common';
 import {LandingRoutingModule} from './landing-routing.module';
 import { LandingComponent } from './components/landing/landing.component';
 import {PreviewComponent} from "./components/preview/preview.component";
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @NgModule({
   declarations: [
     PreviewComponent,
-    LandingComponent
+    LandingComponent,
+    AboutMeComponent
   ],
   imports: [CommonModule, LandingRoutingModule],
   providers: []
